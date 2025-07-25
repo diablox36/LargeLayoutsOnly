8893221399a5db1fb99247853d6aefa5e9f64437
diablox36 <thomas.rivest@proton.me>
2025-07-25 18:34:04 -0400

    Added a check for mods that add their own layout desk

--- LargeLayoutOnly.cs
+++ LargeLayoutOnly.cs
@@ -1,24 +1,26 @@
-﻿using Kitchen;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Kitchen;
 using KitchenData;
 using KitchenMods;
-using System.Collections.Generic;
-using System.Reflection;
 using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
+using static Kitchen.CreateLayoutSlots;
 
 namespace LargeLayoutsOnly
 {
-	[UpdateBefore(typeof(HandleLayoutRequests))]
-	public class FilterLayoutUpgrades : FranchiseSystem, IModSystem
-	{
-		private EntityQuery LayoutUpgrades;
+    [UpdateBefore(typeof(HandleLayoutRequests))]
+    public class FilterLayoutUpgrades : FranchiseSystem, IModSystem
+    {
+        private EntityQuery LayoutUpgrades;
 
-		protected override void Initialise()
-		{
-			base.Initialise();
-			LayoutUpgrades = GetEntityQuery(typeof(CLayoutUpgrade));
-		}
+        protected override void Initialise()
+        {
+            base.Initialise();
+            LayoutUpgrades = GetEntityQuery(typeof(CLayoutUpgrade));
+        }
 
         protected override void OnUpdate()
         {
@@ -35,43 +37,53 @@ namespace LargeLayoutsOnly
         }
     }
 
-	[UpdateAfter(typeof(CreateOffice))]
-	public class CreateLayoutSlotsPatch : FranchiseFirstFrameSystem, IModSystem
-	{
-		private EntityQuery LayoutSizeUpgrades;
-		private CreateLayoutSlots LayoutSlotsSystem;
+    [UpdateAfter(typeof(CreateOffice))]
+    public class CreateLayoutSlotsPatch : FranchiseFirstFrameSystem, IModSystem
+    {
+        private EntityQuery LayoutSizeUpgrades;
+        private EntityQuery LayoutSlots;
+        private CreateLayoutSlots LayoutSlotsSystem;
 
-		protected override void Initialise()
-		{
-			base.Initialise();
-			LayoutSizeUpgrades = GetEntityQuery(typeof(CUpgradeExtraLayout));
-			LayoutSlotsSystem = World.GetExistingSystem<CreateLayoutSlots>();
+        protected override void Initialise()
+        {
+            base.Initialise();
+            LayoutSizeUpgrades = GetEntityQuery(typeof(CUpgradeExtraLayout));
+            LayoutSlots = GetEntityQuery(typeof(CLayoutSlot));
+            LayoutSlotsSystem = World.GetExistingSystem<CreateLayoutSlots>();
 
-			if (LayoutSlotsSystem != null)
-			{
-				LayoutSlotsSystem.Enabled = false;
-			}
-		}
+            if (LayoutSlotsSystem != null)
+            {
+                LayoutSlotsSystem.Enabled = false;
+            }
+        }
 
-		protected override void OnUpdate()
-		{
-			MethodInfo method = typeof(CreateLayoutSlots).GetMethod("CreateMapSource", BindingFlags.Instance | BindingFlags.NonPublic);
+        protected override void OnUpdate()
+        {
+            using (NativeArray<Entity> layoutSlots = LayoutSlots.ToEntityArray(Allocator.Temp))
+            {
+                if (layoutSlots.Any())
+                {
+                    return;
+                }
+            }
 
-			Vector3 office = LobbyPositionAnchors.Office;
-			List<Vector3> list = new List<Vector3>
-			{
-				new Vector3(-0f, 0f, -5f),
-				new Vector3(-1f, 0f, -5f),
-				new Vector3(-2f, 0f, -5f),
-				new Vector3(-3f, 0f, -5f),
-				new Vector3(-4f, 0f, -5f),
-				new Vector3(-4f, 0f, -4f)
-			};
+            MethodInfo method = typeof(CreateLayoutSlots).GetMethod("CreateMapSource", BindingFlags.Instance | BindingFlags.NonPublic);
 
-			for (int i = 0; i < Mathf.Min(6, 4 + LayoutSizeUpgrades.CalculateEntityCount()); i++)
-			{
-				method.Invoke(LayoutSlotsSystem, new object[] { office + list[i] });
-			}
-		}
-	}
+            Vector3 office = LobbyPositionAnchors.Office;
+            var list = new List<Vector3>
+            {
+                new Vector3(-0f, 0f, -5f),
+                new Vector3(-1f, 0f, -5f),
+                new Vector3(-2f, 0f, -5f),
+                new Vector3(-3f, 0f, -5f),
+                new Vector3(-4f, 0f, -5f),
+                new Vector3(-4f, 0f, -4f)
+            };
+
+            for (int i = 0; i < Mathf.Min(6, 4 + LayoutSizeUpgrades.CalculateEntityCount()); i++)
+            {
+                method?.Invoke(LayoutSlotsSystem, new object[] { office + list[i] });
+            }
+        }
+    }
 }
